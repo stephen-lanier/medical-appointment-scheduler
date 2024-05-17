9af767e09183f4abc42d4ef0fdadccb7eda77a7d
stephen-lanier <102939601+stephen-lanier@users.noreply.github.com>
2024-05-16 18:39:23 -0700

    feat: proof of database interface

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import {getCourses, getRegistrations} from "./server";
+import { getCourses, getRegistrations } from "./server";
 
 export default async function Home() {
 
@@ -7,14 +7,11 @@ export default async function Home() {
     let registrationsData = await getRegistrations();
 
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-                <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-                    Courses Info:
-                </p>
-                <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+        <main className="font-mono text-slate-800 p-5 bg-slate-50">
+            <div className="">
+                <div className="">
                     <a
-                        className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+                        className=""
                         href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
                         target="_blank"
                         rel="noopener noreferrer"
@@ -31,94 +28,56 @@ export default async function Home() {
                     </a>
                 </div>
             </div>
-            <div class="relative overflow-x-auto">
-                <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                    <thead>
-                        <tr>
-                            <th scope='col' className='px-6 py-3'>Course ID</th>
-                            <th scope='col' className='px-6 py-3'>Course Name</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {coursesData.map(x => {
-                            return (<tr>
-                                <td className='px-6 py-4'>{x.course_id}</td>
-                                <td className='px-6 py-4'>{x.subject_name}</td>
-                            </tr>);
-                        })}
-                    </tbody>
-                </table>
+
+
+            <div className="py-10">
+                <h1 className="block-inline uppercase tracking-widest text-3xl p-10">Courses</h1>
+                <div className="relative overflow-x-auto shadow-md sm:rounded-lg mx-5">
+                    <table className="w-full text-base text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                        <thead className=" text-gray-700 uppercase bg-gray-200 dark:bg-gray-700 dark:text-gray-400 p-0">
+                            <tr>
+                                <th scope='col' className='px-6 py-3'>Course ID</th>
+                                <th scope='col' className='px-6 py-3'>Course Name</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {coursesData.map(x => {
+                                return (<tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                    <td className='px-6 py-3'>{x.course_id}</td>
+                                    <td className='px-6 py-3'>{x.subject_name}</td>
+                                </tr>);
+                            })}
+                        </tbody>
+                    </table>
+                </div>
             </div>
 
-            <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-                <a
-                    href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <h2 className={`mb-3 text-2xl font-semibold`}>
-                        Docs{" "}
-                        <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                            -&gt;
-                        </span>
-                    </h2>
-                    <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-                        Find in-depth information about Next.js features and API.
-                    </p>
-                </a>
-
-                <a
-                    href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <h2 className={`mb-3 text-2xl font-semibold`}>
-                        Learn{" "}
-                        <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                            -&gt;
-                        </span>
-                    </h2>
-                    <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-                        Learn about Next.js in an interactive course with&nbsp;quizzes!
-                    </p>
-                </a>
-
-                <a
-                    href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <h2 className={`mb-3 text-2xl font-semibold`}>
-                        Templates{" "}
-                        <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                            -&gt;
-                        </span>
-                    </h2>
-                    <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-                        Explore starter templates for Next.js.
-                    </p>
-                </a>
-
-                <a
-                    href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <h2 className={`mb-3 text-2xl font-semibold`}>
-                        Deploy{" "}
-                        <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                            -&gt;
-                        </span>
-                    </h2>
-                    <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-                        Instantly deploy your Next.js site to a shareable URL with Vercel.
-                    </p>
-                </a>
+            <div className="py-10">
+                <h1 className="block-inline uppercase tracking-widest text-3xl p-10">Registrations</h1>
+                <div className="relative overflow-x-auto shadow-md sm:rounded-lg mx-5">
+                    <table className="w-full text-base text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                        <thead className=" text-gray-700 uppercase bg-gray-200 dark:bg-gray-700 dark:text-gray-400 p-0">
+                            <tr>
+                                <th scope='col' className='px-6 py-3'>Student ID</th>
+                                <th scope='col' className='px-6 py-3'>Group ID</th>
+                                <th scope='col' className='px-6 py-3'>Date</th>
+                                <th scope='col' className='px-6 py-3'>Time</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {registrationsData.map(x => {
+                                return (<tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                    <td className='px-6 py-3'>{x.student_id}</td>
+                                    <td className='px-6 py-3'>{x.group_id}</td>
+                                    <td className='px-6 py-3'>{`${x.date}`}</td>
+                                    <td className='px-6 py-3'>{x.time}</td>
+                                </tr>);
+                            })}
+                        </tbody>
+                    </table>
+                </div>
             </div>
+
         </main>
     );
 }
