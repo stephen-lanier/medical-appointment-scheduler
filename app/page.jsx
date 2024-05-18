ad1dcc94df88d38d1fd6d68e98a5b99cbf604da3
stephen-lanier <102939601+stephen-lanier@users.noreply.github.com>
2024-05-18 16:59:42 -0700

    feat: added heroicon package

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import { ArrowRightIcon } from '@heroicons/react/24/outline'
 
 export default async function Home() {
     return (
@@ -16,16 +17,15 @@ export default async function Home() {
             <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
                 <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
                     {/* <div className={styles.shape} /> */}
-                    <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal font-mono uppercase`}>
-                        <strong>Medical Appointment Scheduler</strong> <br />
-                        E Team <br /> CPSC 5021 <br /> Spring 2024
+                    <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal font-mono lowercase`}>
+                        <strong>Medical Appointment Scheduler</strong> - E Team - CPSC 5021 - Spring 2024
                     </p>
                     <Link
                         href="/dashboard"
-                        className="flex items-center gap-5 self-start rounded-lg bg-slate-400 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-slate-300 md:text-base"
+                        className="flex items-center gap-5 self-start rounded-lg bg-slate-400 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-slate-300 md:text-base tracking-widest"
                     >
                         <span>Log in</span>
-                        {/* <ArrowRightIcon className="w-5 md:w-6" /> */}
+                        <ArrowRightIcon className="w-5 md:w-6 stroke-2" />
                     </Link>
                 </div>
                 <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
